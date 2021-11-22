3879871224c4164ff2340fc97302549c6ac1180f
haikalgnz <haikalofc8@gmail.com>
2021-11-22 12:49:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
-global.owner = ['62895372305081','19014445701'] // Letakan nomor kamu disini
-global.mods = ['62895372305081'] // Moderator?
+global.owner = ['6289696454872','62896763806592'] // Letakan nomor kamu disini
+global.mods = ['6289696454872'] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -36,12 +36,12 @@ global.APIKeys = { // APIKey nya disini
 
 // Sticker WM
 global.packname = 'ᴘᴇʀʙᴀɴʏᴀᴋ ʙᴇʀsʏᴜᴋᴜʀ ʙᴜᴋᴀɴɴʏᴀ ɪɴsᴇᴄᴜʀᴇ'
-global.author = 'RLP27'
+global.author = 'IKAL'
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© Laksmana27' //change the watermark (beberapa harus manual)
+global.watermark = '© HAIKAL' //change the watermark (beberapa harus manual)
 global.image = 'https://i.ibb.co/dDNpMkV/1566867082-How-to-know-if-my-Whats-App-is-HACKED-intervened-cloned-768x431.jpg' //change the image
 global.thumbfoto = 'https://i.ibb.co/Yt5TDyj/IMG-20211106-110117-148.jpg'
 global.sumberurl = 'https://youtu.be/0YF8vecQWYs'
