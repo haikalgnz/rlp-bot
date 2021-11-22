95d79b38de2b859cfb64b63db7f86f88875823c7
haikalgnz <haikalofc8@gmail.com>
2021-11-22 12:50:53 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 function handler(m) {
   m.reply('Chat jika penting, harap tidak spam.')
-  this.sendContact(m.chat, '19014445701@s.whatsapp.net', 'RLP-BOT', m)
+  this.sendContact(m.chat, '6289696454872@s.whatsapp.net', 'RLP-BOT', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
